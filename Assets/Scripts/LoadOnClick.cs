c9b4f0f1fe15b8cf802d0fef7fe2dd4f62bd85a2
Skido <marc.villain@epita.fr>
2017-02-01 13:43:47 +0100

    [RESET] Trying to fix issue

---
+++ Assets/Scripts/LoadOnClick.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadOnClick : MonoBehaviour
+{
+    public void LoadButton(int button)
+    {
+        Application.LoadLevel(button);
+    }
+}
