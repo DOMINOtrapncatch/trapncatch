043f9ee8ce438f95839f54de7d8b35f9894ca686
Chiwps <julien.da-cunha@epita.fr>
2017-02-01 19:01:23 +0100

    [MENU] DA CUNHA JULIEN Optimisation de la fonction de changement de scène (SceneManager beaucoup plus opti niveau vitesse/cpu etc) Ajout de la fonction Quit() dans le script LoadOnClick.cs pour quitter le jeu lors de l'appui sur un bouton. <<!!!!!------ Ne pas toucher, c'est normal si ce boutton ne fonctionne pas quand on lance le projet à travers unity, il faut le compiler avant ------!!!!!>> Changement de certains nom de GameObjects pour être plus conventionnel. Changement du système de choix de missons solo

--- Assets/Scripts/LoadOnClick.cs
+++ Assets/Scripts/LoadOnClick.cs
@@ -1,10 +1,99 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using System.Collections;
 
 public class LoadOnClick : MonoBehaviour
 {
-    public void LoadButton(int button)
+    /*
+     * -------------
+     * Created by Wonja
+     * Modified by Julien 
+     * -------------
+     * TODO: EDIT THIS FILE WHEN ALL SCENES WILL BE IMPLEMENTED (LaunchMission)
+     */
+
+    // Go from one scene to another scene specified by his id
+    public void ChangeScene(int sceneId)
     {
-        Application.LoadLevel(button);
+        SceneManager.LoadScene(sceneId);
+    }
+
+    // Close the game
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    // For the solo menu (true == right button, false == left button)
+    public void SelectMission(bool direction)
+    {
+        Text Mission = GameObject.Find("MissionLabel").GetComponent<Text>();
+        switch(Mission.text)
+        {
+            case "Chapitre 0: Une souris intriguante":
+                if (direction)
+                    Mission.text = "Chapitre 1 : Le chat de gouttière";
+                else
+                    Mission.text = "Chapitre 5 : To determine";
+                break;
+            case "Chapitre 1 : Le chat de gouttière":
+                if (direction)
+                    Mission.text = "Chapitre 2 : L'initiation";
+                else
+                    Mission.text = "Chapitre 0: Une souris intriguante";
+                break;
+            case "Chapitre 2 : L'initiation":
+                if (direction)
+                    Mission.text = "Chapitre 3 : De nouveaux arrivants";
+                else
+                    Mission.text = "Chapitre 1 : Le chat de gouttière";
+                break;
+            case "Chapitre 3 : De nouveaux arrivants":
+                if (direction)
+                    Mission.text = "Chapitre 4 : To determine";
+                else
+                    Mission.text = "Chapitre 2 : L'initiation";
+                break;
+            case "Chapitre 4 : To determine":
+                if (direction)
+                    Mission.text = "Chapitre 5 : To determine";
+                else
+                    Mission.text = "Chapitre 3 : De nouveaux arrivants";
+                break;
+            case "Chapitre 5 : To determine":
+                if (direction)
+                    Mission.text = "Chapitre 0: Une souris intriguante";
+                else
+                    Mission.text = "Chapitre 4 : To determine";
+                break;
+        }
+    }
+
+    // Load the selected mission
+    public void LaunchMission()
+    {
+        Text Mission = GameObject.Find("MissionLabel").GetComponent<Text>();
+        switch (Mission.text)
+        {
+            case "Chapitre 0: Une souris intriguante":
+                SceneManager.LoadScene(0);
+                break;
+            case "Chapitre 1 : Le chat de gouttière":
+                SceneManager.LoadScene(0);
+                break;
+            case "Chapitre 2 : L'initiation":
+                SceneManager.LoadScene(0);
+                break;
+            case "Chapitre 3 : De nouveaux arrivants":
+                SceneManager.LoadScene(0);
+                break;
+            case "Chapitre 4 : To determine":
+                SceneManager.LoadScene(0);
+                break;
+            case "Chapitre 5 : To determine":
+                SceneManager.LoadScene(0);
+                break;
+        }
     }
 }
