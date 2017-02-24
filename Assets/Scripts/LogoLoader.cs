1b5880c689dec37a909771fe7712784da57b0a4b
Wonj4 <wonja.na-ngille@epita.fr>
2017-02-24 19:04:25 +0100

    [Splash] slpash domino

---
+++ Assets/Scripts/LogoLoader.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LogoLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("Countdown");
+	
+	}
+
+    private IEnumerator Countdown()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(0);
+    }
+}
