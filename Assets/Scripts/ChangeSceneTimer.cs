280aabd4fe5f987bccafc32d4dcc09338c1122a0
Wonj4 <wonja.na-ngille@epita.fr>
2017-02-24 17:07:10 +0100

    [splash] debut splash

---
+++ Assets/Scripts/ChangeSceneTimer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Timers;
+
+public class ChangeSceneTimer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+
+    void StartMain()
+    {
+        Timer timer = new Timer();
+        timer.Elapsed += LoadNextScene;
+        timer.Interval = 800;
+        timer.Enabled = true;
+    }
+
+    private void LoadNextScene(object source, ElapsedEventArgs e)
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
