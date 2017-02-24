d818e3fa3bf58e2af33919ece1cbde622898b08c
Chiwps <julien.da-cunha@epita.fr>
2017-02-24 18:25:24 +0100

    [Menu] More pretty design

---
+++ Assets/Scripts/Touchable.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using System.Collections;
+
+public class Touchable : Text
+{
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+}
+
+[CustomEditor(typeof(Touchable))]
+public class Touchable_Editor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        // Do nothing
+    }
+}
