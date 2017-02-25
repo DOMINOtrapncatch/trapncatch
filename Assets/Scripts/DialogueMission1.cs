e2f3f8fd4187abe330571bcd6b9554dd07d98048
Wonj4 <wonja.na-ngille@epita.fr>
2017-02-25 19:24:07 +0100

    [visual novel] visual novel mission 0

---
+++ Assets/Scripts/DialogueMission1.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class DialogueMission1 : MonoBehaviour {
+
+    public Text dialogueStr;
+    string dialogueTxt;
+    int click;
+	// Use this for initialization
+	void Start ()
+    {
+        dialogueStr = GameObject.Find("DialogueText").GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        dialogueStr.text = dialogueTxt;
+        if (click == 0)
+            dialogueTxt = "*MIAOUU*";
+        if (click == 1)
+            dialogueTxt = "Oh une souris!";
+        if (click == 1)
+            dialogueTxt = "Je vais un peu m'amuser avec elle...";
+    }
+
+    public void Proceed()
+    {
+        click++;
+        click += 0;
+    }
+}
