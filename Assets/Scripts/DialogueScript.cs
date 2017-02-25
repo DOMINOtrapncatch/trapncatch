f5e5e49c26568c00213f0253a46539d3f459c369
Chiwps <julien.da-cunha@epita.fr>
2017-02-25 21:28:32 +0100

    [Dialogue] Optimisation du script de manière à le rendre universel

---
+++ Assets/Scripts/DialogueScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class DialogueScript : MonoBehaviour
+{
+    public string dialogueLabel;
+    public string[] dialogueTexts;
+    public int sceneId;
+    Text dialogueText;
+    int click = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        dialogueText = GameObject.Find(dialogueLabel).GetComponent<Text>();
+        Proceed();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space))
+            Proceed();
+    }
+
+    public void Proceed()
+    {
+        if (click < dialogueTexts.Length)
+            dialogueText.text = dialogueTexts[click];
+        else
+            SceneManager.LoadScene(sceneId);
+        ++click;
+    }
+}
