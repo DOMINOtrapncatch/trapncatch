a68cde59990540cff05f3f0b3335466adc219951
worldmurder <anspake@gmail.com>
2017-02-27 00:14:55 +0100

    [MOVE] script de mouvement + camera intégré operationnel manque juste de prendre en compte la vitesse des chats

---
+++ Assets/Scripts/CharacterController_AnS/thirdperson_camera.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class thirdperson_camera : MonoBehaviour {
+
+    public Transform player;
+    public float lookSmooth = 0.09f; //how fast we look at out target
+    public Vector3 offSetFromTarget = new Vector3(0, 2, -4); // distance camera -> player //ou try 0,-6,-8
+    public float camrot_x = 10; //rotation de la camera sur l'axe x
+
+    private Vector3 destination = Vector3.zero; //where we moving to
+    private thirdperson_move charcontroller;
+    private float rotatevelo = 0; //ne pas oublier de le prendre en compte tmtc
+
+
+
+    void Start()
+    {
+        SetCameraTarget(player);
+    }
+
+
+    //pour coller la camera a un new player
+    //a utiliser pour le swap
+    //wonja je t'ai a l'oeil
+    void SetCameraTarget(Transform p)
+    {
+        player = p;
+
+        if (player != null)
+        {
+            if (player.GetComponent<thirdperson_move>())
+            {
+                charcontroller = player.GetComponent<thirdperson_move>();
+            }
+            else
+            {
+                Debug.LogError("check presence charac controller + call amandine");
+            }
+
+        }
+        else
+        {
+            Debug.LogError("add player 'cause its null/empty + call amandine");
+        }
+    }
+
+
+    void LateUpdate()
+    {
+        /*
+        mouvement de la camera basé sur mouvement du chara
+        donc on utilise un lateupdate vu que les moves de chara se font dans un update et fixedupdate
+        */
+        MoveToTarget();
+        LookAtTarget();
+    }
+
+    //movement de la camera
+    void MoveToTarget()
+    {
+        destination = charcontroller.get_targetrot * offSetFromTarget;
+        destination += player.position;
+        transform.position = destination;
+       
+    }
+
+    void LookAtTarget()//mouvement uniquement sur l'axe y tmtc
+    {
+        float eulerangle_y = Mathf.SmoothDampAngle(transform.eulerAngles.y,player.eulerAngles.y,ref rotatevelo,lookSmooth);
+        transform.rotation = Quaternion.Euler(transform.eulerAngles.x, eulerangle_y, 0);
+    }
+}
---
+++ Assets/Scripts/CharacterController_AnS/thirdperson_move.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class thirdperson_move : MonoBehaviour {
+
+    //ON OUBLIT PAS LE RIGIDBODY SVP
+
+    public float inputdelay = 0.1f; //on peut enlever yolo
+    public float forwardvelo = 12;
+    public float rotatevelo = 100;
+
+    private Quaternion targetrot;
+    private Rigidbody rigbody;
+
+    private float forwardinput;
+    private float turninput;
+    
+    //getter pour targetrot
+    //je le laisse au cas ou mais au final je l'utilise pas
+    public Quaternion get_targetrot
+    {
+        get { return targetrot; }
+    }
+
+    private void Start()
+    {
+        targetrot = transform.rotation;
+
+        if (GetComponent<Rigidbody>())
+        {
+            rigbody = GetComponent<Rigidbody>();
+        }
+        else
+        {
+            Debug.LogError("rigidbody inexistant + Call Amandine");
+        }
+        forwardinput = 0;
+        turninput = 0;
+    }
+
+    private void Update()
+    {
+        GetInput();
+        Turn();
+    }
+
+    private void FixedUpdate() //manage moves that required physics (jump,run)
+    {
+        Run();
+    }
+
+    private void GetInput()
+    {
+        forwardinput = Input.GetAxis("Vertical");//pour vertical/hori check unity properties
+        // -1 < forwardinput < 1
+
+        turninput = Input.GetAxis("Horizontal");
+    }
+
+
+    //moves
+    private void Run()
+    {
+        //deadzone = zone de delai
+        //gestion deadzone
+        if (Mathf.Abs(forwardinput) > inputdelay)
+        {
+            //on fait bouger le charac
+            rigbody.velocity = transform.forward * forwardinput * forwardvelo;
+            /*
+                forwardinput peut etre soit >0 soit <0, c'est ce qui gere si on avance ou on recule            
+            */
+        }
+        else
+        {
+            //sinon le charact bouge pas
+            rigbody.velocity = Vector3.zero;
+        }
+    }
+
+    private void Turn()
+    {
+        //gestiond deadzone
+        if (Mathf.Abs(turninput) > inputdelay)
+        {
+            targetrot *= Quaternion.AngleAxis(rotatevelo * turninput * Time.deltaTime, Vector3.up);
+            //meme delire que run() -> cest le signe de turninput qui gere si on tourne a gauche ou a droite
+        }
+        transform.rotation = targetrot;
+    }
+}
