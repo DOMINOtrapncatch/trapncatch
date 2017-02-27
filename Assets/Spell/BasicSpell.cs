1ef39a2666174ff63745413877fd7a407bc966e7
worldmurder <anspake@gmail.com>
2017-02-27 19:10:23 +0100

    [SPELL] spell are back thanks to amandine's backup

---
+++ Assets/Spell/BasicSpell.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicSpell : Spell {
+
+    
+	public BasicSpell (Cat cat, KeyCode input) : base(cat, input)
+    {
+        base.mana_cost = 0;
+        base.recovery_max = (150 - cat.speed) / 100;
+        base.recovery_time = base.recovery_max;
+
+        base.cat = cat;
+        base.input = input;
+    }
+
+    public override void Activate() //attaque basique
+    {
+        //attaque
+		Vector3 pos = transform.position;
+		for(int i = 0; i < cat.nearEnemy.Count;i++)
+		{
+			Vector3 vec = cat.nearEnemy[i].transform.position;
+			Vector3 direction = vec - pos;
+			if(Vector3.Dot(direction, transform.forward) < 0.7)
+			{
+				Destroy (cat.nearEnemy[i]);
+			}
+		}
+
+        //animation + call marc
+        
+    }
+}
---
+++ Assets/Spell/Spell.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.Remoting;
+
+public abstract class Spell : MonoBehaviour {
+
+    public int recovery_time;//delai between spell
+    public int mana_cost;
+    public int recovery_max;
+    public Cat cat;
+    public KeyCode input;
+    public Spell(Cat cat, KeyCode input)
+    {
+        this.cat = cat;
+        this.input = input;
+    }
+
+    //delai de recovery
+    //coup en mana
+    //check
+    public bool CanUse()
+    {
+		if(Input.GetKey (input) && recovery_time == recovery_max && mana_cost <=  cat.mana)
+        {
+            recovery_time = 0;
+            cat.mana -= mana_cost;
+            return true;
+            
+        }
+
+        return false;
+    }
+
+	abstract public void Activate();
+     
+	
+}
