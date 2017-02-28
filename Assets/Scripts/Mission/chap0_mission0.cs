f3076bc359e59312af6106c25a900a40bf2d9233
worldmurder <anspake@gmail.com>
2017-02-28 03:54:25 +0100

    [SCRIPT] ILS SONT LA MAIS SERRENT A RIEN MDR

---
+++ Assets/Scripts/Mission/chap0_mission0.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class chap0_mission0 : MonoBehaviour {
+
+    public HUDManager hudmanager;
+    public Cat cat;
+    public Input input;
+
+    public bool tooltip1;
+    
+    public bool tooltip3;
+    public bool tooltip4;
+    // Use this for initialization
+    void Start()
+    {
+        //premier tooltip
+        hudmanager.SetObjective("Se déplacer (T.D. ou ZQSD)");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //en fonction des checktooltip validés, on affiche les tooltip suivants
+        //c'pas opti a modif
+        if(CheckTooltip1())
+        {
+            hudmanager.SetObjective("Clic gauche pour charger une attaque");
+            if(CheckTooltip2())
+            {
+                hudmanager.SetObjective("Poursuivre la souris");
+                if(CheckTooltip3())
+                {
+                    hudmanager.SetObjective("Se rendre jusqu'à la porte");
+                    if(CheckTooltip4())
+                    {
+                        //win
+                        //passage au chapitre suivant 
+                    }
+                }
+            }
+        }
+    }
+
+    bool CheckTooltip1()
+    {
+        if(Input.GetKeyDown(KeyCode.Z) || 
+            Input.GetKeyDown(KeyCode.Q) || 
+            Input.GetKeyDown(KeyCode.S) || 
+            Input.GetKeyDown(KeyCode.D) || 
+            Input.GetKeyDown(KeyCode.UpArrow) ||
+            Input.GetKeyDown(KeyCode.DownArrow) ||
+            Input.GetKeyDown(KeyCode.LeftArrow) ||
+            Input.GetKeyDown(KeyCode.RightArrow) )
+        {
+            return tooltip1 &= true;
+        }
+
+        return tooltip1 &= false;
+    }
+
+    bool CheckTooltip2()
+    {
+        if(Input.GetMouseButton(0))//left click
+        {
+            return true;
+        }
+        return false;
+    }
+
+    bool CheckTooltip3()
+    {
+        //pourchassez la souris jusqu'a un point B
+
+        //si collider chat is triggered by collider souris
+        return true;
+    }
+
+    bool CheckTooltip4()
+    {
+        // se rendre jusqu'a a porte -> loading scene menu et debloque chap 1
+
+        //when vector3 chat  == vector3 position de la sortie 
+        return true;
+    }
+
+
+}
---
+++ Assets/Scripts/Mission/chap1_mission1.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class chap1_mission1 : MonoBehaviour {
+    public Cat cat;
+    public Mouse mouse;
+    public HUDManager hudmanager;
+	// Use this for initialization
+	void Start () {
+        //position du chat au dessus de la poubelle
+        //spawn le chat a cet endroit 
+        //sinon on s'en ballec vu qu'il y'a le bug immobilisant
+        hudmanager.SetObjective("Attaquez sur la souris");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(CheckTooltip1())
+        {
+            hudmanager.SetObjective("Achevez la souris(Coup Special)");
+            if (mouse.life <=0 && !CheckTooltip2())
+            {
+                mouse.life = 1;//freez pv de la souris a 1 tant que il utilise pas une att sppeciale
+            }
+            else if(CheckTooltip2() && mouse.life == 0)//test si il a bien fatality la souris 
+            {
+                //win
+                //retour menu selection chapitre
+            }
+        }
+	}
+
+    bool CheckTooltip1()
+    {
+        if (mouse.maxLife != mouse.life)//aka elle s'est faite attaquer au moins une fois
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    bool CheckTooltip2()
+    {
+        //on va check si le mana du chat a diminué au moins une fois
+        if(cat.maxMana != cat.mana)
+        {
+            if(mouse.life == 0 || mouse.life ==1)//pour gerer des bugs eventuels tmtc
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+}
