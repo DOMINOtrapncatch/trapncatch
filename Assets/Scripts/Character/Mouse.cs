9b4275ffa05c20f7d3795630c339363a176d21f9
Marc Villain <marc.villain@epita.fr>
2017-04-10 18:11:25 +0200

    [IA] Pathfinding: path smoothing.

--- Assets/Scripts/Character/Mouse.cs
+++ Assets/Scripts/Character/Mouse.cs
@@ -3,42 +3,91 @@ using System.Collections;
 
 public class Mouse : Character
 {
+	const float minPathUpdateTime = .2f;
+	const float pathUpdateMoveThreshold = .5f;
+
 	public Transform target;
-	Vector3[] path;
-	int targetIndex;
+	public float turnSpeed = 3;
+	public float turnDst = 5;
+	public float stoppingDst = 10;
+
+	Path path;
 
 	void Start()
 	{
-		PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
+		StartCoroutine(UpdatePath());
 	}
 
-	public void OnPathFound(Vector3[] newPath, bool pathSuccessful)
+	public void OnPathFound(Vector3[] waypoints, bool pathSuccessful)
 	{
 		if(pathSuccessful)
 		{
-			path = newPath;
-			StopCoroutine("FollowPath");
+			path = new Path(waypoints, transform.position, turnDst, stoppingDst);
+			StopCoroutine("FollowPath"); // In case it alwready exists
 			StartCoroutine("FollowPath");
+		}
+	}
+
+	IEnumerator UpdatePath()
+	{
+		// Handle level loading trouble maker
+		if (Time.timeSinceLevelLoad < .3f)
+			yield return new WaitForSeconds(.3f);
+		
+		PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
+
+		float sqrMoveThreshold = pathUpdateMoveThreshold * pathUpdateMoveThreshold;
+		Vector3 targetPosOld = target.position;
+
+		while(true)
+		{
+			yield return new WaitForSeconds(minPathUpdateTime);
+
+			// Update only if moved a certain dist (this is here for performance issues)
+			if((target.position - targetPosOld).sqrMagnitude > sqrMoveThreshold)
+			{
+				PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
+				targetPosOld = target.position;
+			}
 		}
 	}
 
 	IEnumerator FollowPath()
 	{
-		Vector3 currentWaypoint = path[0];
+		bool followingPath = true;
+		int pathIndex = 0;
+		transform.LookAt(path.lookPoints[0]);
 
-		while(true)
+		float speedPercent = 1;
+
+		while (followingPath)
 		{
-			if(transform.position == currentWaypoint)
+			Vector2 pos2D = new Vector2(transform.position.x, transform.position.z);
+			while(path.turnBoundaries[pathIndex].HasCrossedLine(pos2D))
 			{
-				targetIndex++;
-				if(targetIndex >= path.Length)
+				if (pathIndex == path.finishLineIndex)
 				{
-					yield break; // Exit the coroutine
+					followingPath = false;
+					break;
 				}
-				currentWaypoint = path[targetIndex];
+				else
+				{
+					pathIndex++;
+				}
 			}
 
-			transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
+			if (followingPath)
+			{
+				if(pathIndex >= path.slowDownIndex && stoppingDst > 0)
+				{
+					speedPercent = Mathf.Clamp01(path.turnBoundaries[path.finishLineIndex].DistanceFromPoint(pos2D) / stoppingDst);
+					followingPath = !(speedPercent < 0.01f);
+				}
+
+				Quaternion targetRotation = Quaternion.LookRotation(path.lookPoints[pathIndex] - transform.position);
+				transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * turnSpeed);
+				transform.Translate(Vector3.forward * Time.deltaTime * speed * speedPercent, Space.Self);
+			}
 
 			yield return null; // Move to the next frame
 		}
@@ -48,27 +97,15 @@ public class Mouse : Character
 	{
 		if(path != null)
 		{
-			for (int i = targetIndex; i < path.Length; i++)
-			{
-				Gizmos.color = Color.black;
-				Gizmos.DrawCube(path[i], Vector3.one);
-
-				if(i == targetIndex)
-				{
-					Gizmos.DrawLine(transform.position, path[i]);
-				}
-				else
-				{
-					Gizmos.DrawLine(path[i - 1], path[i]);
-				}
-			}
+			path.DrawWithGizmos();
 		}
 	}
 
 	/*void Update()
 	{
-		
-		POOR PATHFINDING
+		[POOR PATHFINDING]
+		=> I KEEP THIS IF WE WANT TO RE-IMPLEMENT IT (BUT DAMN CHANGE THE CALCULATION IT IS BAADDDD)
+
 		if (Vector3.SqrMagnitude(targetPositions [currentPosition].transform.position - transform.position) <= 0.02)
 		{
 			currentPosition = (currentPosition + 1) % targetPositions.Count;
---
+++ Assets/Scripts/PathFinding/Line.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Line {
+
+	const float verticalLineGradient = 1e5f;
+
+	float gradient;
+	float y_intercept;
+	Vector2 pointOnLine_1;
+	Vector2 pointOnLine_2;
+
+	float gradientPerpendicular;
+
+	bool approachSide;
+
+	public Line(Vector2 pointOnLine, Vector2 pointPerpendicularToLine)
+	{
+		float dx = pointOnLine.x - pointPerpendicularToLine.x;
+		float dy = pointOnLine.y - pointPerpendicularToLine.y;
+
+		gradientPerpendicular = dx == 0 ? verticalLineGradient : dy / dx;
+		gradient = gradientPerpendicular == 0 ? verticalLineGradient : -1 / gradientPerpendicular;
+		y_intercept = pointOnLine.y - gradient * pointOnLine.x;
+
+		pointOnLine_1 = pointOnLine;
+		pointOnLine_2 = pointOnLine + new Vector2(1, gradient);
+
+		approachSide = false;
+		approachSide = GetSide(pointPerpendicularToLine);
+	}
+
+	bool GetSide(Vector2 point)
+	{
+		return (point.x - pointOnLine_1.x) * (pointOnLine_2.y - pointOnLine_1.y) > (point.y - pointOnLine_1.y) * (pointOnLine_2.x - pointOnLine_1.x);
+	}
+
+	public bool HasCrossedLine(Vector2 point)
+	{
+		return GetSide(point) != approachSide;
+	}
+
+	public float DistanceFromPoint(Vector2 point)
+	{
+		float yInterceptPerpendicular = point.y - gradientPerpendicular * point.x;
+		float intersectX = (yInterceptPerpendicular - y_intercept) / (gradient - gradientPerpendicular);
+		float intersectY = gradient * intersectX + y_intercept;
+
+		return Vector2.Distance(point, new Vector2(intersectX, intersectY));
+	}
+
+	public void DrawWithGizmos(float length)
+	{
+		Vector3 lineDir = new Vector3(1, 0, gradient).normalized;
+		Vector3 lineCenter = new Vector3(pointOnLine_1.x, 0, pointOnLine_1.y) + Vector3.up;
+		Gizmos.DrawLine(lineCenter - lineDir * length / 2f, lineCenter + lineDir * length / 2f);
+	}
+}
