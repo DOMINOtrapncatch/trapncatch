aa123e91bda42cb3218b66945660118302a1b170
worldmurder <anspake@gmail.com>
2017-04-16 14:01:13 +0200

    [FILES] add prefab for the mouse, allowed to delete

--- Assets/Scripts/HUD/Mission/Mission1.cs
+++ Assets/Scripts/HUD/Mission/Mission1.cs
@@ -14,11 +14,15 @@ public class Mission1 : MonoBehaviour
     public Image spellBar3;
     public Image spellBar4;
     public Text objective;
+    private int tooltip1;
+    private int tooltip2;
 
     // Use this for initialization
     void Start ()
     {
         myHUD = new HUDManager(player, healthBar, manaBar, spellBar1, spellBar2, spellBar3, spellBar4, objective);
+        tooltip1 = 0;
+        tooltip2 = 0;
         //position du chat au dessus de la poubelle
         //spawn le chat a cet endroit 
         //sinon on s'en ballec vu qu'il y'a le bug immobilisant
@@ -27,43 +31,44 @@ public class Mission1 : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update () {
-	    /*if(CheckTooltip1())
+	    if(CheckTooltip1(ref tooltip1) >= 1)
         {
-            myHUD.SetObjective("Achevez la souris(Coup Special)");
-            if (mouse.life <=0 && !CheckTooltip2())
+            myHUD.SetObjective("Achevez la souris (E)");
+            if (mouse.life <=0 && CheckTooltip2(ref tooltip2) == 0) //if on kill la souris sans capacité special
             {
                 mouse.life = 1;//freez pv de la souris a 1 tant que il utilise pas une att sppeciale
+
+
             }
-            else if(CheckTooltip2() && mouse.life == 0)//test si il a bien fatality la souris 
+            else if(CheckTooltip2(ref tooltip2) >=1 && mouse.life <= 0)//test si il a bien fatality la souris 
             {
                 //win
                 //retour menu selection chapitre
+
             }
-        }*/
+        }
 	}
 
-    bool CheckTooltip1()
+    int CheckTooltip1(ref int tooltip1)
     {
         if (mouse.maxLife != mouse.life)//aka elle s'est faite attaquer au moins une fois
         {
-            return true;
+            ++tooltip1;
+            
         }
 
-        return false;
+        return tooltip1;
     }
 
-    bool CheckTooltip2()
+    int CheckTooltip2(ref int tooltip2)
     {
         //on va check si le mana du chat a diminué au moins une fois
         if(player.maxMana != player.mana)
         {
-            if(mouse.life == 0 || mouse.life ==1)//pour gerer des bugs eventuels tmtc
-            {
-                return true;
-            }
+            ++tooltip2;
         }
 
-        return false;
+        return tooltip2;
     }
 
 }
