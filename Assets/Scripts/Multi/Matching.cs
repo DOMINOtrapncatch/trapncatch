696c04b85bc5824b87766064021923b3f3efc925
worldmurder <anspake@gmail.com>
2017-04-16 17:25:51 +0200

    [SCRIPT] testing the low level api and matchmaking but only keeping llapi is enough

---
+++ Assets/Scripts/Multi/Matching.cs
@@ -0,0 +1,155 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Types;
+using UnityEngine.Networking.Match;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Matching : NetworkBehaviour {
+
+    //matchmaker
+    List<MatchInfoSnapshot> matchlist = new List<MatchInfoSnapshot>();
+    bool matchlist_created;
+    bool matchlist_joined;
+    MatchInfo matchinfo;
+    string matchname = "New Garden"; //modif
+    NetworkMatch netmatch;
+
+    //connection
+    int hostid = -1;
+    List<int> connectIDs = new List<int>(); //server has multiple connection
+    byte[] receive_buff;
+    string netmess = "Hello DOMINO";
+    string lastmess = "";
+    NetworkWriter writer;
+    NetworkReader reader;
+    bool connectok;
+
+    const int serverport = 25000;
+    const int maxsizemess = 65535;
+
+    void Awake()
+    {
+        netmatch = gameObject.AddComponent<NetworkMatch>();
+    }
+
+    void Start()
+    {
+        receive_buff = new byte[maxsizemess];
+        writer = new NetworkWriter();
+
+        //if localhost -> en gros il va search pour les autres joueurs si on reste que sur une machine ok
+        Application.runInBackground = true;
+    }
+
+    void OnApplicationQuit()
+    {
+        NetworkTransport.Shutdown();
+    }
+
+    void OnGUI() //customisable tacru
+    {
+        if(string.IsNullOrEmpty(Application.cloudProjectId))
+        {
+            GUILayout.Label("Set up the project first");
+        }
+        else
+        {
+            GUILayout.Label("Ok :" + Application.cloudProjectId);
+
+        }
+
+        if(matchlist_joined)
+        {
+            GUILayout.Label(matchname + "is here");
+
+        }
+        else if(matchlist_created)
+        {
+            GUILayout.Label(matchname + "created");
+        }
+
+        GUILayout.Label("Connection : " + connectok);
+
+        if (matchlist_created || matchlist_joined)
+        {
+            GUILayout.Label("Address :" + matchinfo.address + " and Port :" + matchinfo.port);//info server
+            GUILayout.Label("MatchID :" + matchinfo.networkId + " clientId :" + matchinfo.nodeId);//info matchmaker && client
+
+
+
+            #region message manager GUI
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Out message");
+            netmess = GUILayout.TextField(netmess);
+            GUILayout.EndHorizontal();
+            GUILayout.Label("Last message : " + lastmess);
+
+            if (connectok && GUILayout.Button("Send message"))
+            {
+                writer.SeekZero();
+                writer.Write(netmess);
+                byte error;
+                //envoie du message a tous les clients
+                for (int i = 0; i < connectIDs.Count; ++i)
+                {
+                    NetworkTransport.Send(hostid, connectIDs[i], 0, writer.AsArray(), writer.Position, out error);
+                    if ((NetworkError)error != NetworkError.Ok)
+                    {
+                        Debug.Log("Failed in sending message : " + (NetworkError)error);
+                    }
+                }
+            }
+            #endregion
+
+            //shutting down
+            if (GUILayout.Button("Exit"))
+            {
+                netmatch.DropConnection(matchinfo.networkId, matchinfo.nodeId, 0, OnConnectionDropped);
+            }
+
+        }
+         //..... on a besoin que d'un room nous en soit donc on s'arrete la
+        foreach(var match in matchlist)
+        {
+            if(GUILayout.Button(match.name))
+            {
+                //netmatch.JoinMatch(match.networkId, "", "", "", 0, 0, OnMatchJoined);
+            }
+        }
+    }
+
+    public void OnConnectionDropped(bool success,string extendedInfo)
+    {
+        Debug.Log("Connection has been dropped");
+        NetworkTransport.Shutdown();
+        hostid = -1;
+        connectIDs.Clear();
+        matchinfo = null;
+        matchlist_created = false;
+        matchlist_joined = false;
+        connectok = false;
+    }
+
+    public virtual void OnMatchCreate(bool success, string extendedInfo,MatchInfo matchinfo2)
+    {
+        if(success)
+        {
+            Debug.Log("Create match success");
+            Utility.SetAccessTokenForNetwork(matchinfo.networkId, matchinfo.accessToken);
+
+            matchlist_created = true;
+            matchinfo = matchinfo2;
+
+            //OnStartServer(matchinfo.address, matchinfo.port, matchinfo.networkId);
+            
+        }
+        else
+        {
+            Debug.LogError("Create match failed : " + extendedInfo);
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/Multi/Multi.cs
@@ -0,0 +1,132 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class Multi : NetworkBehaviour {
+
+    /*
+     1) opening socket
+     2) connect to another socket
+     3) send messages
+     4) receive messages
+
+     */
+    int channelID;
+    int socketID;
+    int socketPort = 7777; // NE PAS OUBLIER DE MODIF CECI
+    string host = "127.0.0.1"; //idem
+    int connectID;
+	void Start () {
+        
+        //setup
+        NetworkTransport.Init();
+        ConnectionConfig config = new ConnectionConfig();
+
+        //init channel for futur send/receive actions
+        channelID = config.AddChannel(QosType.Reliable); //less faster but secu
+        
+        //set up max numbers of connection for the socket tmtc
+        int maxConnect = 8;
+        HostTopology topo = new HostTopology(config, maxConnect);
+
+        //init socket
+        socketID = NetworkTransport.AddHost(topo, socketPort);
+        Debug.Log("Socket open :" + socketID);
+
+        Connect();
+	}
+	
+	public void Connect()
+    {
+        byte error;
+
+        //NE PAS OUBLIER DE MODIF
+        connectID = NetworkTransport.Connect(socketID, "localhost", socketPort, 0, out error);
+        NetworkError neterror = (NetworkError)error;
+        if(neterror != NetworkError.Ok)
+        {
+            Debug.Log("Nope");
+        }
+        else
+        {
+            Debug.Log("Connected to Server :" + connectID);
+        }
+        
+        
+    }
+
+    public void Disconnect()
+    {
+        byte error;
+        NetworkTransport.Disconnect(socketID, connectID, out error);
+    }
+
+    public void SendSocketMessage()
+    {
+        byte error;
+        byte[] buffer = new byte[1024];
+        Stream stream = new MemoryStream(buffer);
+        BinaryFormatter format = new BinaryFormatter();
+        format.Serialize(stream, "HelloDOMINO");
+
+        NetworkTransport.Send(socketID, connectID, channelID, buffer, 1024, out error);
+
+
+    }
+
+
+    void Update()
+    {
+        //Listening/receive message
+
+        int hostid_rec;
+        int connectID_rec;
+        int channelID_rec;
+        byte[] buffer_rec = new byte[1024];
+        int dataSize;
+        byte error;
+
+        NetworkEventType netEvent = NetworkTransport.Receive(out hostid_rec, out connectID_rec, out channelID_rec, buffer_rec, 1024, out dataSize, out error);
+
+        /*
+         Enum with 4 types
+         1) Nothing
+         2) ConnectionEvent -> socket up
+         3) DataEvent -> message received
+         4) DisconnectEvent
+         */
+
+        switch(netEvent)
+        {
+            case NetworkEventType.Nothing:
+                break;
+
+            case NetworkEventType.ConnectEvent:
+                
+                Debug.Log("New connection received");
+                //ADD player prefab, display text etc etc
+                
+                break;
+
+            case NetworkEventType.DataEvent:
+
+                Stream stream = new MemoryStream(buffer_rec);
+                BinaryFormatter format = new BinaryFormatter();
+                string message = format.Deserialize(stream) as string;
+                Debug.Log("New message received");
+
+                break;
+
+            case NetworkEventType.DisconnectEvent:
+
+                Debug.Log("Remote client disconnected");
+                //destroy player etc 
+
+                break;
+
+        }
+    }
+
+}
