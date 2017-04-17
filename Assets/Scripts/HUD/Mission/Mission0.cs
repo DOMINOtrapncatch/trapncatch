2b81e022991bdc754f38f453b82c0082abd8f874
worldmurder <anspake@gmail.com>
2017-04-17 22:54:46 +0200

    [ETC]

--- Assets/Scripts/HUD/Mission/Mission0.cs
+++ Assets/Scripts/HUD/Mission/Mission0.cs
@@ -21,8 +21,10 @@ public class Mission0 : MonoBehaviour
 	public string messageTooltip1 = "Se déplacer";
 	public string messageTooltip2 = "Left Click pour attaquer";
 	public string messageTooltip3 = "Poursuivre la souris";
+    public string messageTooltip4 = "Se rendre jusqu'à la porte";
 	int tooltip1 = 0;
 	int tooltip2 = 0;
+    bool CheckTooltip3 = false;
 
 	// Use this for initialization
 	void Start()
@@ -79,7 +81,12 @@ public class Mission0 : MonoBehaviour
 
 	void OnTriggerEnter(Collider box)
 	{
-		if (box.tag == "Enemy" && CheckTooltip2(false))
-			AutoFade.LoadLevel(9, .3f, .3f, Color.black);
-	}
+        if (box.tag == "Enemy" && CheckTooltip2(false))
+        {
+            myHUD.SetObjective(messageTooltip3);
+            CheckTooltip3 = true;
+        }
+        else if (box.tag == "Collider" && CheckTooltip3)
+            AutoFade.LoadLevel(9, .3f, .3f, Color.black);
+    }
 }
