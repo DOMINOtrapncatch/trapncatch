77ef8b32fee44a00d9e2b8ecd70d9a54db11f45e
Marc Villain <marc.villain@epita.fr>
2017-04-20 16:55:42 +0200

    [ANIM] Better animation done.

--- Assets/Scripts/Character/Controller/CatAnimController.cs
+++ Assets/Scripts/Character/Controller/CatAnimController.cs
@@ -6,12 +6,29 @@ public class CatAnimController : MonoBehaviour {
 	private Animator anim;
 
 	void Start () {
-		anim = gameObject.GetComponentInChildren<Animator>();
+		anim = gameObject.GetComponent<Animator>();
 	}
 
 	void Update (){
-		float move = Input.GetAxis ("Vertical");
-		move *= Input.GetKey (KeyCode.LeftShift) ? 0.4f : 0.2f;
-		anim.SetFloat("Speed", Input.GetKey (KeyCode.Space) ? 1f : move);
+		//float move = Input.GetAxis ("Vertical");
+		//move *= Input.GetKey (KeyCode.LeftShift) ? 0.4f : 0.2f;
+		//anim.SetFloat("Speed", Input.GetKey (KeyCode.Space) ? 1f : move);
+
+		if(Input.GetButton("up") || Input.GetButton("right") || Input.GetButton("down") || Input.GetButton("left"))
+		{
+			anim.SetInteger("State", (Input.GetButton("attack") || Input.GetButton("attack_special") ? 4 : (Input.GetButton("Jump") ? 3 : (Input.GetButton("run") ? 2 : 1))));
+		}
+		else if(Input.GetButton("Jump"))
+		{
+			anim.SetInteger("State", 3);
+		}
+		else if(Input.GetButton("attack") || Input.GetButton("attack_special"))
+		{
+			anim.SetInteger("State", 4);
+		}
+		else
+		{
+			anim.SetInteger("State", 0);
+		}
 	}
 }
