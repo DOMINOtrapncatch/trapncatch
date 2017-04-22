1032bab650f03bdfcc30dada9bc4c2e5792090df
Marc Villain <marc.villain@epita.fr>
2017-04-22 16:51:49 +0200

    [ADD] Added HealSpell for special cat, with prefab particule effect.

---
+++ Assets/Scripts/Spell/SpecialCat/HealSpell.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class HealSpell : Spell {
+
+	[Header("Spell Data")]
+
+	public ParticleSystem particle;
+	[Range(0, 100)]
+	public int lifeAmount = 20;
+
+	public override void Activate()
+	{
+		particle.Stop();
+		particle.transform.position = cat.transform.position;
+		particle.Play();
+
+		cat.Life += lifeAmount;
+		if (cat.Life > cat.MaxLife)
+			cat.Life = cat.MaxLife;
+	}
+}
