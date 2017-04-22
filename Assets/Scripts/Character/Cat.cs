7b0db4ed858cf966343846947c55e299f2da4604
Marc Villain <marc.villain@epita.fr>
2017-04-22 19:48:23 +0200

    [IA] Added cat pathfinding wouhouu

--- Assets/Scripts/Character/Cat.cs
+++ Assets/Scripts/Character/Cat.cs
@@ -2,6 +2,7 @@
 using UnityEngine.UI;
 using System.Collections.Generic;
 using System.Net;
+using System.Collections;
 
 public class Cat : Character
 {
@@ -28,8 +29,26 @@ public class Cat : Character
 	public List<Spell> spells;
 	[HideInInspector]
 	public List<GameObject> nearEnemy = new List<GameObject>();
+	//[HideInInspector]
+	public List<GameObject> aroundEnemy = new List<GameObject>();
 	[HideInInspector]
 	public int enemyKillCount = 0;
+
+	[Header("Pathfinding Settings")]
+	public bool isPathfindingActive = false;
+	public CatType catType = CatType.NORMAL;
+
+	const float minPathUpdateTime = .2f;
+	const float pathUpdateMoveThreshold = .5f;
+
+	public List<Transform> targets = new List<Transform>();
+	public bool randomTarget = true;
+	public float turnSpeed = 3;
+	public float turnDst = 5;
+	public float stoppingDst = 10;
+
+	int targetIndex;
+	Path path;
 
 	void Update()
 	{
@@ -87,8 +106,232 @@ public class Cat : Character
 		// Destroy corresponding object
 		Destroy(nearEnemy[enemyIndex]);
 
+		// Remove enemy from lists
+		nearEnemy.Remove(nearEnemy[enemyIndex]);
+		aroundEnemy.Remove(nearEnemy[enemyIndex]);
+
 		// Increment number of enemies killes
 		++enemyKillCount;
 	}
+
+	/*
+	 *  =================
+	 *   | PATHFINDING |
+	 *  ==================
+	 */
+
+	void Start()
+	{
+		// Update randomizer seed
+		Random.InitState(System.DateTime.Now.Millisecond);
+
+		// Init start and target based on random or not
+		if(targets.Count > 0)
+		{
+			if (randomTarget)
+			{
+				targetIndex = Random.Range(1, targets.Count - 1);
+
+				int startIndex;
+				if (targets.Count > 1)
+				{
+					do
+					{
+						startIndex = Random.Range(1, targets.Count - 1);
+					}
+					while (startIndex == targetIndex);
+				}
+				else
+				{
+					startIndex = 0;
+				}
+
+				transform.position = targets[startIndex].position;
+			}
+			else
+			{
+				targetIndex = 1;
+				transform.position = targets[0].position;
+			}
+		}
+
+		StartCoroutine(UpdatePath());
+	}
+
+	public void OnPathFound(Vector3[] waypoints, bool pathSuccessful)
+	{
+		if (pathSuccessful)
+		{
+			// Get success path
+			path = new Path(waypoints, transform.position, turnDst, stoppingDst);
+
+			StopCoroutine("FollowPath"); // In case coroutine alwready exists
+			StartCoroutine("FollowPath");
+		}
+	}
+
+	IEnumerator UpdatePath()
+	{
+		// Handle level loading trouble maker
+		if (Time.timeSinceLevelLoad< .3f)
+			yield return new WaitForSeconds(.3f);
+
+		while(true)
+		{
+			if(isPathfindingActive)
+			{
+				if (targets.Count > 0 && targetIndex > 0)
+				{
+					PathRequestManager.RequestPath(new PathRequest(transform.position, targets[targetIndex].position, OnPathFound));
+
+					float sqrMoveThreshold = pathUpdateMoveThreshold * pathUpdateMoveThreshold;
+					Vector3 targetPosOld = targets[targetIndex].position;
+
+					while (isPathfindingActive)
+					{
+						yield return new WaitForSeconds(minPathUpdateTime);
+
+						if (targetIndex< 0)
+							break;
+
+						// Update only if moved a certain dist (this is here for performance issues)
+						if ((targets[targetIndex].position - targetPosOld).sqrMagnitude > sqrMoveThreshold)
+						{
+							PathRequestManager.RequestPath(new PathRequest(transform.position, targets[targetIndex].position, OnPathFound));
+							targetPosOld = targets[targetIndex].position;
+						}
+					}
+				}
+				else if(aroundEnemy.Count > 0)
+				{
+					PathRequestManager.RequestPath(new PathRequest(transform.position, aroundEnemy[0].transform.position, OnPathFound));
+
+					float sqrMoveThreshold = pathUpdateMoveThreshold * pathUpdateMoveThreshold;
+					Vector3 targetPosOld = aroundEnemy[0].transform.position;
+
+					while (isPathfindingActive)
+					{
+						yield return new WaitForSeconds(minPathUpdateTime);
+
+						if (aroundEnemy.Count == 0)
+							break;
+						
+						// Update only if moved a certain dist (this is here for performance issues)
+						if ((aroundEnemy[0].transform.position - targetPosOld).sqrMagnitude > sqrMoveThreshold)
+						{
+							PathRequestManager.RequestPath(new PathRequest(transform.position, aroundEnemy[0].transform.position, OnPathFound));
+							targetPosOld = aroundEnemy[0].transform.position;
+						}
+					}
+				}
+				else
+				{
+					DecideNextMove();
+					yield return new WaitForSeconds(minPathUpdateTime);
+				}	
+			}
+			else
+			{
+				yield return new WaitForSeconds(minPathUpdateTime);
+			}
+		}
+	}
+
+	IEnumerator FollowPath()
+	{
+		bool followingPath = true;
+		int pathIndex = 0;
+		transform.LookAt(path.lookPoints[0]);
+
+		float speedPercent = 1;
+
+		while (followingPath && isPathfindingActive)
+		{
+			Vector2 pos2D = new Vector2(transform.position.x, transform.position.z);
+			while (path.turnBoundaries[pathIndex].HasCrossedLine(pos2D))
+			{
+				if (pathIndex == path.finishLineIndex)
+				{
+					followingPath = false;
+					break;
+				}
+				else
+				{
+					pathIndex++;
+				}
+			}
+
+			if (followingPath)
+			{
+				if (pathIndex >= path.slowDownIndex && stoppingDst > 0)
+				{
+					speedPercent = Mathf.Clamp01(path.turnBoundaries[path.finishLineIndex].DistanceFromPoint(pos2D) / stoppingDst);
+					followingPath = !(speedPercent < 0.2f);
+				}
+
+				Quaternion targetRotation = Quaternion.LookRotation(path.lookPoints[pathIndex] - transform.position);
+				transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * turnSpeed);
+				transform.Translate(Vector3.forward * Time.deltaTime * speed * speedPercent, Space.Self);
+			}
+
+			// Priority to the nearest enemy
+			if (targetIndex != -1 && catType == CatType.NORMAL && aroundEnemy.Count > 0)
+			{
+				targetIndex = -1;
+				break;
+			}
+
+			yield return null; // Move to the next frame
+		}
+
+		DecideNextMove();
+	}
+
+	private void DecideNextMove()
+	{
+		// Decide of the next move
+		switch (catType)
+		{
+			case CatType.NORMAL:
+				// If there are enemies near me, follow it. Else, go somewere you know.
+				if (aroundEnemy.Count > 0)
+				{
+					targetIndex = -1;
+				}
+				else
+				{
+					if (randomTarget)
+					{
+						int startIndex = 0;
+						if (targets.Count > 1)
+						{
+							do
+							{
+								startIndex = Random.Range(0, targets.Count - 1);
+							}
+							while (startIndex == targetIndex);
+						}
+						targetIndex = startIndex;
+					}
+					else
+					{
+						targetIndex = 1;
+					}
+				}
+				break;
+		}
+	}
+
+	public void OnDrawGizmos()
+	{
+		if (path != null)
+		{
+			path.DrawWithGizmos();
+		}
+	}
 }
 
+public enum CatType
+{
+	NORMAL
+}
---
+++ Assets/Scripts/Character/CatProximityTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatProximityTrigger : MonoBehaviour {
+
+	Cat cat;
+
+	void Start()
+	{
+		cat = GetComponentInParent<Cat>();
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.tag == "Enemy")
+			cat.aroundEnemy.Add(col.gameObject.transform.parent.gameObject);
+	}
+
+	void OnTriggerExit(Collider col)
+	{
+		if (col.gameObject.tag == "Enemy")
+			cat.aroundEnemy.Remove(col.gameObject.transform.parent.gameObject);
+	}
+}
