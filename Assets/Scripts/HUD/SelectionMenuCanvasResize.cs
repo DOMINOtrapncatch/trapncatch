8ad7353199d378f2467c100d1888be2b8d7aa8bd
Skido <marc.villain@epita.fr>
2017-04-25 00:10:20 +0200

    [GUI] Added Multiplayer Selection Panel

---
+++ Assets/Scripts/HUD/SelectionMenuCanvasResize.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectionMenuCanvasResize : MonoBehaviour {
+
+	void Awake ()
+	{
+		// TODO - Resize canvas based on screen size (width variable and Canvas Scale Factor variable)
+
+		RectTransform canvasContent = GetComponentInChildren<RectTransform>();
+	}
+}
