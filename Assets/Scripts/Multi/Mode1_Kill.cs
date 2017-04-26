706e324f2018a0aafc2ce7bdf434f31630c7e9aa
worldmurder <anspake@gmail.com>
2017-04-26 18:57:26 +0200

    [SCRIPT] mode1 begin

--- Assets/Scripts/Multi/Mode1_Kill.cs
+++ Assets/Scripts/Multi/Mode1_Kill.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 public class Mode1_Kill : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	/*
+     * spaw nbr mouse
+     * get list of nbr mouse and deathcounter
+     * despawn when killed and check with death counter
+     * deathcounter ok = finish game
+     * 
+     * each cat has their own death counter
+     * sort for the biggest one -> winner
+     * 
+     * 
+     */
 }
