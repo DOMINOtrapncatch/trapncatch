77b5d9584a2f3a424d1ac120d6f2d83e6b2da743
Skido <marc.villain@epita.fr>
2017-04-27 01:56:48 +0200

    [ADD] Starting on Magic Ball attack for special cat.

---
+++ Assets/Scripts/Spell/SpecialCat/MagicBallSpell.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class MagicBallSpell : Spell
+{
+	public GameObject magicBallParticule;
+
+	public float magicBallSpeed = 200f;
+
+	public override void Activate()
+	{
+		StartCoroutine("SendProjectile");
+	}
+
+	void SendProjectile()
+	{
+		GameObject magicBall = (GameObject)Instantiate(magicBallParticule, cat.transform.position + new Vector3(0, 0.2f, 0), cat.transform.rotation);
+
+		magicBall.GetComponent<Rigidbody>().AddForce(cat.transform.forward * magicBallSpeed);
+
+		// TODO - 1. Check for collision
+		//        2. Stop ball animation
+		//        3. Start explosion animtion
+		//        4. Handle surrounding damage
+		//        5. (OPTION) Handle surrounding velocity
+	}
+}
