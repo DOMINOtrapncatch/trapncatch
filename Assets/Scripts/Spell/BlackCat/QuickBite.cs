9614f251c529601a4df54581c8ce2efd2562a909
Chiwps <julien.da-cunha@epita.fr>
2017-04-27 12:11:17 +0200

    [Spell] Updated

---
+++ Assets/Scripts/Spell/BlackCat/QuickBite.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuickBite : Spell
+{
+    [Header("QuickBite settings")]
+    [Range(0, 100)]
+    public float dashDuration = 3F;
+    public Vector3 direction = new Vector3(0, 0, .1F);
+    public ParticleSystem particle;
+
+    public override void Activate()
+    {
+        StartCoroutine(Effect());
+    }
+
+    IEnumerator Effect()
+    {
+
+        if (!particle.isPlaying)
+            particle.Play();
+
+        Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+        float duration = dashDuration;
+
+        while (duration > 0)
+        {
+            rb.AddForce(transform.forward + direction);
+            duration -= .1F;
+
+            yield return new WaitForSeconds(.1F);
+        }
+
+        cat.spells[0].Activate();
+
+        particle.Stop();
+
+        yield return null;
+    }
+}
