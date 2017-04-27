ccf09c3c8d840fed862d291986562d02df3b3c99
worldmurder <anspake@gmail.com>
2017-04-27 22:55:33 +0200

    [CAP2] done and working

--- Assets/Scripts/Spell/AverageCat/CatFood.cs
+++ Assets/Scripts/Spell/AverageCat/CatFood.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using System;
 
 public class CatFood : Spell {
 
     public GameObject foodParticle;
-    int restraint = 10; //secondes
-    RigidbodyConstraints rgb;
+    List<GameObject> nearEnemySave;
+    List<float> speedEnemy;
+    float restraint = 10; //secondes
     //manaCost = 20
     
 
@@ -14,29 +16,41 @@ public class CatFood : Spell {
     {
         //get the other cat.position and freeze it
         //check with timer
+        speedEnemy = new List<float>();
         StartCoroutine("Freeze");
 
     }
 
     IEnumerator Freeze()
     {
-        GameObject particleInit = (GameObject)Instantiate(foodParticle, cat.transform.position, Quaternion.identity);
-        ParticleSystem food = particleInit.GetComponent<ParticleSystem>();
-        food.Play();
-
-        if (cat.nearEnemy.Count > 0)
+        nearEnemySave = cat.aroundEnemy;
+        if (nearEnemySave.Count > 0)
         {
-            rgb = cat.nearEnemy[0].GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
-        }
 
-        for(int i = 0; i <= restraint; ++i)
-        {
-            print(i);
-            yield return new WaitForSeconds(1.0f);
+            GameObject particleInit = (GameObject)Instantiate(foodParticle, cat.transform.position, Quaternion.identity);
+            ParticleSystem food = particleInit.GetComponent<ParticleSystem>();
+            food.Play();
+
+            //freez
+            foreach (GameObject enemy in nearEnemySave)
+            {
+                Character chara = enemy.GetComponent<Character>();
+                speedEnemy.Add(chara.Speed);
+                chara.Speed = 0;
+            }
+
+            yield return new WaitForSeconds(restraint);
+
+            //unfreez
+            for (int i = 0; i < nearEnemySave.Count; ++i)
+            {
+                nearEnemySave[i].GetComponent<Character>().Speed = speedEnemy[i];
+            }
+            //end particle (noo)
+            nearEnemySave.Clear();
+            speedEnemy.Clear();
+            food.Stop();
+            Destroy(particleInit, 0.1f);
         }
-        rgb = cat.nearEnemy[0].GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
-        //end particle (noo)
-        food.Stop();
-        Destroy(particleInit, 0.1f);
     }
 }
