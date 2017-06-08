b14e94647b979839be3c49028c9595af9c104e05
worldmurder <anspake@gmail.com>
2017-06-08 23:48:38 +0200

    [UNET] stuff

--- Assets/Scripts/Multi/LoadPrefab.cs
+++ Assets/Scripts/Multi/LoadPrefab.cs
@@ -1,8 +1,9 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
 using System.Collections;
+using System.Collections.Generic;
 
-public class LoadPrefab : MonoBehaviour {
+public class LoadPrefab : NetworkLobbyManager {
     /*
     public Object[] cat_prefabs;
     public NetworkManager netMan;
@@ -28,9 +29,26 @@ public class LoadPrefab : MonoBehaviour {
         }   
     }
     */
-
-
+    private Dictionary<int, int> currentPlayer;
     
+    public override GameObject OnLobbyServerCreateLobbyPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        if (!currentPlayer.ContainsKey(conn.connectionId))
+            currentPlayer.Add(conn.connectionId, 0);
+        return base.OnLobbyServerCreateLobbyPlayer(conn, playerControllerId);
+    }
 
-    
+    public void SetPlayerCatLobby(NetworkConnection conn)
+    {
+        if (currentPlayer.ContainsKey(conn.connectionId))
+            currentPlayer[conn.connectionId] = PlayerPrefs.GetInt("ChoosenCat");
+    }
+    public override GameObject OnLobbyServerCreateGamePlayer(NetworkConnection conn, short playerControllerId)
+    {
+        int index = currentPlayer[conn.connectionId];
+        GameObject player = (GameObject) GameObject.Instantiate(spawnPrefabs[index], startPositions[conn.connectionId].position,Quaternion.identity);
+        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+        return player;
+    }
+
 }
