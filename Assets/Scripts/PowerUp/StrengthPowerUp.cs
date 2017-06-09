43b51ac006e63395c95d83e3aca6248198b72c55
Skido <marc.villain@epita.fr>
2017-06-09 19:07:57 +0200

    [ADD] Power Ups added and finished. NOTE: Add 'Cat' tag to every cats.

---
+++ Assets/Scripts/PowerUp/StrengthPowerUp.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StrengthPowerUp : PowerUp
+{
+	public float strengthIncrease = 10.0f;
+	public float strengthTimeout = 15.0f;
+
+	protected override void Activate(Character enemy)
+	{
+		enemy.StartCoroutine(Strength(enemy));
+	}
+
+	private IEnumerator Strength(Character enemy)
+	{
+		gameObject.SetActive(false);
+
+		enemy.Attack += strengthIncrease;
+		yield return new WaitForSeconds(strengthTimeout);
+		enemy.Attack -= strengthIncrease;
+
+		Destroy(gameObject);
+	}
+}
