ae8f3870fca4bb1aa12034a7da0b14302a44c5c2
Wonj4 <wonja.na-ngille@epita.fr>
2017-06-10 23:47:30 +0200

    [ADD] menu pause + menu option + menu echouer

---
+++ Assets/Scripts/HUD/ChangeVolume.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ChangeVolume : MonoBehaviour {
+
+    public Slider slideVolume;
+    public AudioSource musicSource;
+
+    public void SetVolume()
+    {
+        musicSource.volume = slideVolume.value;
+    }
+	
+}
---
+++ Assets/Scripts/HUD/PauseScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PauseScript : MonoBehaviour {
+
+    GameObject PauseMenu;
+    bool paused;
+
+	// Use this for initialization
+	void Start () {
+        paused = false;
+        PauseMenu = GameObject.Find("PauseMenu");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.Escape))
+        {
+                paused = !paused;
+        }
+
+        if(paused)
+        {
+            PauseMenu.SetActive(true);
+            Time.timeScale = 0;
+        }
+
+        else if(!paused)
+        {
+            PauseMenu.SetActive(false);
+            Time.timeScale = 1;
+        }
+	
+	}
+
+    public void resume()
+    {
+        paused = false;
+    }
+}
