f09129e1927b3eaf859d688921dc6265b095c2ee
Skido <marc.villain@epita.fr>
2017-06-11 07:22:40 +0200

    [ADD] Added start to screept PowerUpManager into Map-Rue

---
+++ Assets/Scripts/PathFinding/PowerUpManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PowerUpManager : MonoBehaviour {
+
+	public List<Transform> powerUpSpawnPoints = new List<Transform>();
+	public List<GameObject> powerUpProfiles = new List<GameObject>();
+
+	GameObject[] placedPowerUps;
+
+	void Start()
+	{
+		placedPowerUps = new GameObject[powerUpSpawnPoints.Count];
+	}
+
+	void Update ()
+	{
+		if(Random.Range(0, 20) == 0)
+		{
+			int randomPlace = Random.Range(0, powerUpSpawnPoints.Count - 1);
+
+			if(placedPowerUps[randomPlace] == null)
+			{
+				//placedPowerUps = powerUpProfiles[]
+			}
+		}
+	}
+}
