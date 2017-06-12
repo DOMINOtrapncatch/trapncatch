0fee8104b71b6c8240dbbc9ee1e1d64b44ce93e7
Skido <marc.villain@epita.fr>
2017-06-12 10:04:03 +0200

    [FIX] Fixed cat food.

--- Assets/Scripts/Spell/AverageCat/CatFood.cs
+++ Assets/Scripts/Spell/AverageCat/CatFood.cs
@@ -6,8 +6,6 @@ using System;
 public class CatFood : Spell {
 
     public GameObject foodParticle;
-    List<GameObject> nearEnemySave;
-    List<float> speedEnemy;
     float restraint = 10; //secondes
     //manaCost = 20
     
@@ -16,51 +14,42 @@ public class CatFood : Spell {
     {
         //get the other cat.position and freeze it
         //check with timer
-
-       speedEnemy = new List<float>();
        StartCoroutine("Freeze");
-        
     }
 
     IEnumerator Freeze()
     {
-        nearEnemySave = cat.nearEnemy;
-         if (nearEnemySave.Count > 0)
-        {
+		Dictionary<Character, float> nearEnemySave = new Dictionary<Character, float>();
 
-            GameObject particleInit = (GameObject)Instantiate(foodParticle, cat.transform.position, Quaternion.identity);
-            ParticleSystem food = particleInit.GetComponent<ParticleSystem>();
-            food.Play();
+		// Start particle effect
+		GameObject particleInit = (GameObject)Instantiate(foodParticle, cat.transform.position + Vector3.up * .25f, foodParticle.transform.rotation);
+
+		if (cat.aroundEnemy.Count > 0)
+		{
             //freez
-            foreach (GameObject enemy in nearEnemySave)
-            {
-                if (enemy != null)
-                {
-                    Character chara = enemy.GetComponent<Character>();
-                    speedEnemy.Add(chara.Speed);
+			foreach (GameObject enemy in cat.aroundEnemy)
+			{
+				// Get character
+				Character chara = enemy.GetComponent<Character>();
+
+				if (enemy != null && !nearEnemySave.ContainsKey(chara))
+				{
+					// Save its data
+					nearEnemySave.Add(chara, chara.Speed);
+
+					// Set its speed
                     chara.Speed = 0;
                 }
             }
-            Debug.Log("near saw 1 : " + nearEnemySave.Count);
-            Debug.Log("saved  : " + speedEnemy.Count);
-            yield return new WaitForSeconds(restraint);
+		}
 
-            Debug.Log("near saw 2 : " + nearEnemySave.Count);
-            Debug.Log("saved 2 : " + speedEnemy.Count);
-            //unfreez
-            for(int i = 0; i < nearEnemySave.Count; ++i)
-            {
-                Character victim = nearEnemySave[i].GetComponent<Character>();
-                victim.Speed = speedEnemy[i];
-                nearEnemySave.RemoveAt(i);
-                speedEnemy.RemoveAt(i);
+		yield return new WaitForSeconds(restraint);
 
-            }
-            //end particle (noo)
-            food.Stop();
-            Destroy(particleInit, 0.1f);
+		//unfreez
+		foreach(KeyValuePair<Character, float> enemy in nearEnemySave)
+			enemy.Key.Speed = enemy.Value;
 
-            
-        }
+		// End particle effect
+		Destroy(particleInit, 0.1f);
     }
 }
