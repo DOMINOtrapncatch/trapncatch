2f450dfe6a15ab9560d83252313c02f8f82e8678
Wonj4 <wonja.na-ngille@epita.fr>
2017-06-13 01:18:33 +0200

    [ADD] script spell gravity

--- Assets/Scripts/Spell/StongCat/Gravity.cs
+++ Assets/Scripts/Spell/StongCat/Gravity.cs
@@ -18,14 +18,25 @@ public class Gravity : Spell
         if (!particle.isPlaying)
             particle.Play();
 
-        foreach(GameObject enemy in cat.aroundEnemy)
+        List<float> speedEnemy = new List<float>();
+        List<GameObject> AroundEnemy = cat.aroundEnemy;
+
+        foreach(GameObject enemy in AroundEnemy)
         {
             Character CatEnemy = (Character) enemy.GetComponent(typeof(Character));
+            speedEnemy.Add(CatEnemy.Speed);
             CatEnemy.Speed = 0;
         }
 
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(4.0f);
 
         particle.Stop();
+
+        for(int i = 0; i< AroundEnemy.Count; i++)
+        {
+            AroundEnemy[i].GetComponent<Character>().Speed = speedEnemy[i];
+        }
+        speedEnemy.Clear();
+        AroundEnemy.Clear();
     }
 }
