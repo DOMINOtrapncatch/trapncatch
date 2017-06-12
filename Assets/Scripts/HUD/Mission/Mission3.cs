cdece42abb644e52af7bccce10663d586f85350e
Wonj4 <wonja.na-ngille@epita.fr>
2017-06-12 18:36:14 +0200

    [ADD] mission 3

---
+++ Assets/Scripts/HUD/Mission/Mission3.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Mission3 : MissionBase
+{
+    // Tooltips variables
+    int tooltip1;
+
+    override public void CheckTooltips()
+    {
+        switch (currentTooltip)
+        {
+            case 1:
+                if (CheckTooltip1())
+                {
+                    AutoFade.LoadLevel(9, .3f, .3f, Color.black);
+                }
+                break;
+
+            case 2:
+                if(!CheckTooltip1())
+                {
+                    myHUD.SetObjective("Achever la souris intéligente");
+                    currentTooltip++;
+                }
+                break;
+        }
+    }
+
+    /*
+	 * Objectif: tuer une souris maligne
+	 */
+    bool CheckTooltip1()
+    {
+        List<GameObject> ListCatEnemy = myHUD.player.nearEnemy;
+        bool stop = false;
+        foreach(GameObject enemy in ListCatEnemy)
+        {
+            Mouse mouse = enemy.GetComponent<Mouse>();
+            if(mouse != null)
+            {
+                if(mouse.mouseType == MouseType.LVL2)
+                {
+                    stop = true;
+                    break;
+                }
+            }
+        }
+        return stop;
+    }
+
+}
