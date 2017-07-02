dca0a3e7f56a8862fb9839571048cb4df1f38013
worldmurder <anspake@gmail.com>
2017-07-03 00:02:34 +0200

    [MIS4]wallah

---
+++ Assets/Scripts/HUD/Mission/Mission4.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using System;
+
+public class Mission4 : MissionBase {
+
+    public string tooltip = "Survivez";
+
+    public List<Cat> players;
+    public List<Mouse> enemies;
+
+    private bool playersAllDead = false;
+
+    //mouse manager 
+    //disons moins de 10 souris meurtrieres
+    public override void CheckTooltips()
+    {
+        throw new NotImplementedException();
+    }
+    
+}
