dd9f0e14168a9dd0549d4294d36c3199900116e2
worldmurder <anspake@gmail.com>
2017-07-04 02:05:43 +0200

    Le commit

---
+++ Assets/Scripts/Misc/MainMenuVolumeCanvas.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuVolumeCanvas : MonoBehaviour {
+
+    //dsl mais je prefere rajouter un script plutot que de casser une bonne architecture x)
+    //pas trop le temps de faire gaffe genre tmtc
+
+    //ceci est un script offrant plein de possibilité genre publicité et tout ça et genre oe et genre la vie cest des choxu a al mdoe de chez onsou
+
+    private GameObject canvas;
+    private bool retry;
+
+    void Start()
+    {
+        canvas = GameObject.Find("PauseMenu");
+        canvas.SetActive(true);
+        retry = false;
+    }
+
+    void Update () {
+
+        DeletePopCanvas();
+	}
+
+    private void DeletePopCanvas()
+    {
+        if (!retry)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                canvas.SetActive(false);
+                retry = true;
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                canvas.SetActive(true);
+                retry = false;
+            }
+        }
+
+        //askip si il a changé d'avis
+
+    }
+}
